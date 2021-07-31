12665d1728256f21384fc7863f16b30c5c2c76d1
Amitendu <amitcse049@gmail.com>
2021-07-31 13:42:10 +0530

    Added Acheivements

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -10,7 +10,8 @@ import { Box, Boxes, BoxNum, BoxText } from "./AcomplishmentsStyles";
 const data = [
   { number: 62, text: "Global Rank in Codechef April Long Challenge" },
   { number: 3, text: "Star Coder in Codechef" },
-  { number: 500, text: "Plus Problems Solved in GFG/Leetcode/Hackerank" },
+  { number: 4, text: "Star Coder in HackerRank" },
+  { number: 500, text: "Plus Problems Solved in GFG and HackerRank" },
 ];
 
 const Acomplishments = () => (
